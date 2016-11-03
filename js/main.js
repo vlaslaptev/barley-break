fd3bdb2fad8affa17346af1de46132d9ecffd5ea
Vlas Laptev <vlaptev@vp.ru>
2016-11-03 13:04:20 +0600

    Add arrow keys control

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var steps = 0;
 var game = false;
 
 var cells = [1, 2, 3, 6, 7, 10, 11, 14, 15, 16];
+var allcells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 var borderLeftCells = [5, 9, 13];
 
 function incrementStep() {
@@ -67,7 +68,7 @@ $_td.on("click", function () {
     }
 });
 
-$(".restart").click(function() {
+$(".restart").click(function () {
     $(".victory").text("");
     fill = [];
     $("#16").text("");
@@ -93,4 +94,61 @@ function checkVictory() {
         return true;
     }
     return false;
+}
+
+$(document).keydown(function (e) {
+    if (e.keyCode > 36 && e.keyCode < 41) {
+        if (!game) {
+            pause_play_clock();
+            game = true;
+        }
+        var currentEmpty = $(".empty");
+        var $_cell = $("#" + getMovedCellId(currentEmpty, e.keyCode));
+        if ($_cell.attr("id")) {
+            incrementStep();
+            currentEmpty.text($_cell.text());
+            currentEmpty.removeClass("empty");
+            $_cell.addClass("empty");
+            $_cell.text("");
+
+
+            if ($("#16").text() == "" && checkVictory()) {
+                $(".victory").text("Победа!");
+                pause_play_clock();
+                game = false;
+            }
+        }
+    }
+});
+
+function getMovedCellId(emptyCell, kode) {
+    var idEmpty = emptyCell.attr('id');
+    if (kode == 37) { //left
+        idEmpty++;
+        if ($.inArray(parseInt(idEmpty), borderLeftCells) != -1) {
+            idEmpty = -1
+        }
+    }
+
+    if (kode == 38) { //up
+        idEmpty = parseInt(idEmpty) + 4;
+        if(!$.inArray(idEmpty, allcells)) {
+            idEmpty = -1
+        }
+    }
+
+    if (kode == 39) { //right
+        if (parseInt($.inArray(parseInt(idEmpty), borderLeftCells)) != -1) {
+            idEmpty = -1
+        } else {
+            idEmpty--;
+        }
+    }
+    if (kode == 40) { //down
+        idEmpty = parseInt(idEmpty) - 4;
+        if($.inArray(parseInt(idEmpty), allcells) == -1) {
+            idEmpty = -1
+        }
+    }
+    return idEmpty;
 }
